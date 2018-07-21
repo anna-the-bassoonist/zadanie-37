bcd42c07fbcdfe6c7597353d9f59299e146826ee
annafagocistka <anafagocistka@gmail.com>
2018-07-22 01:45:19 +0200

    click-dodaj

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('dodaj').addEventListener('click',function(dodajTekst){
+    
+    document.getElementById('tekst').innerHTML = "Akademia 108";
+});
+
+document.getElementById('usun').removeEventListener('click',function(dodajTekst){
+    
+    document.getElementById('tekst').innerHTML = "Akademia 108";
+});
